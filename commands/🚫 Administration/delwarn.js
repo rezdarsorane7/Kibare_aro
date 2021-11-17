e0443bfa6c64baf85b196610ecebaa38e1645bbc
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-11-17 04:36:41 +0300

    Update delwarn.js

--- commands/🚫 Administration/delwarn.js
+++ commands/🚫 Administration/delwarn.js
@@ -3,7 +3,7 @@ var ee = require('../../config/embed.json');
 var config = require('../../config/config.json');
 
 module.exports = {
-    name: 'delwarn',
+    name: '/delwarn',
     aliases: ['paradon'],
     category: '🚫 Administration',
     memberpermissions: ['MANAGE_GUILD'],
