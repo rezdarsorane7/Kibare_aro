c5cc283cbff5228506c1be81546411a83a6482f6
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-11-17 03:55:54 +0300

    Update clearsongs.js

--- commands/🎶 Music/clearsongs.js
+++ commands/🎶 Music/clearsongs.js
@@ -4,7 +4,7 @@ var config = require("../../config/config.json");
 const distube = require("../../utils/distubeClient");
 
 module.exports = {
-  name: "clearsongs",
+  name: "/clearsongs",
   aliases: ["ssg"],
   category: "🎶 Music",
   permissions: " ",
