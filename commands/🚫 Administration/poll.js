9b210031895dc30f0050e7145134d06d7b4e5283
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-11-17 04:32:33 +0300

    Update poll.js

--- commands/🚫 Administration/poll.js
+++ commands/🚫 Administration/poll.js
@@ -3,7 +3,7 @@ var ee = require('../../config/embed.json');
 var config = require('../../config/config.json');
 
 module.exports = {
-    name: 'poll',
+    name: '/poll',
     aliases: ['pl'],
     category: '🚫 Administration',
     memberpermissions: ['ADMINISTRATOR'],
