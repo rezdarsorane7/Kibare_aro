7c292a9d0ec9513a9f4d3f49193dd3eb88de82c1
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-11-17 04:30:01 +0300

    Update setnick.js

--- commands/🚫 Administration/setnick.js
+++ commands/🚫 Administration/setnick.js
@@ -3,7 +3,7 @@ var ee = require('../../config/embed.json');
 var config = require('../../config/config.json');
 
 module.exports = {
-    name: 'setnick',
+    name: '/setnick',
     aliases: ['nickname'],
     category: '🚫 Administration',
     memberpermissions: ['MANAGE_NICKNAMES'],
