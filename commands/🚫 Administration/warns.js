a29860afcafdd1551693951ebd2ddef1bfde592c
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-11-17 04:24:55 +0300

    Update warns.js

--- commands/🚫 Administration/warns.js
+++ commands/🚫 Administration/warns.js
@@ -3,7 +3,7 @@ var ee = require('../../config/embed.json');
 var config = require('../../config/config.json');
 
 module.exports = {
-    name: 'warns',
+    name: '/warns',
     aliases: ['warnings'],
     category: '🚫 Administration',
     memberpermissions: ['MANAGE_GUILD'],
