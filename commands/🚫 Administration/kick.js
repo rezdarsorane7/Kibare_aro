04b5135b9f0eefbed06a9040b4062ad748db2f6f
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-11-17 04:34:12 +0300

    Update kick.js

--- commands/🚫 Administration/kick.js
+++ commands/🚫 Administration/kick.js
@@ -3,7 +3,7 @@ var ee = require('../../config/embed.json');
 var config = require('../../config/config.json');
 
 module.exports = {
-    name: 'kick',
+    name: '/kick',
     aliases: ['nikal ja'],
     category: '🚫 Administration',
     memberpermissions: ['KICK_MEMBERS'],
