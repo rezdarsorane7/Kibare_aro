9442852a4d60b6341bb9b6bead2572912b656cb6
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-11-17 03:55:09 +0300

    Update loop.js

--- commands/🎶 Music/loop.js
+++ commands/🎶 Music/loop.js
@@ -4,7 +4,7 @@ var config = require("../../config/config.json");
 const distube = require("../../utils/distubeClient");
 
 module.exports = {
-  name: "loop",
+  name: "/loop",
   aliases: ["s"],
   category: "🎶 Music",
   permissions: " ",
