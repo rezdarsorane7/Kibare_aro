2a7ed877c690e803f9d7b57ab79199000243ae31
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-11-17 04:33:28 +0300

    Update massban.js

--- commands/🚫 Administration/massban.js
+++ commands/🚫 Administration/massban.js
@@ -3,7 +3,7 @@ var ee = require('../../config/embed.json');
 var config = require('../../config/config.json');
 
 module.exports = {
-    name: 'massban',
+    name: '/massban',
     aliases: ['bhagja'],
     category: '🚫 Administration',
     memberpermissions: ['BAN_MEMBERS'],
