b5f503edcb5bae745c1b49cc13857913574e91e3
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-11-17 04:23:37 +0300

    Update avatar.js

--- commands/🔰 Info/avatar.js
+++ commands/🔰 Info/avatar.js
@@ -3,7 +3,7 @@ var ee = require('../../config/embed.json');
 var config = require('../../config/config.json');
 
 module.exports = {
-    name: 'avatar',
+    name: '/avatar',
     aliases: ['av', 'pfp'],
     category: '🔰 Info',
     memberpermissions: [],
