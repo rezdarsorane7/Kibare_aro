c812866d7f5569f7a06aa59982a9220e5f1d9224
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-11-17 03:45:40 +0300

    Update stop.js

--- commands/🎶 Music/stop.js
+++ commands/🎶 Music/stop.js
@@ -4,7 +4,7 @@ var config = require("../../config/config.json");
 const distube = require("../../utils/distubeClient");
 
 module.exports = {
-  name: "stop",
+  name: "/ stop /",
   aliases: ["s"],
   category: "🎶 Music",
   permissions: " ",
