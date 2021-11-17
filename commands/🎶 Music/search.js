74d532ca406e07d117ac06ec3c703f611c1a0a66
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-11-17 03:50:22 +0300

    Update search.js

--- commands/🎶 Music/search.js
+++ commands/🎶 Music/search.js
@@ -4,7 +4,7 @@ var config = require("../../config/config.json");
 const distube = require("../../utils/distubeClient");
 
 module.exports = {
-  name: "search",
+  name: "/search",
   aliases: ["sr"],
   category: "🎶 Music",
   permissions: "",
