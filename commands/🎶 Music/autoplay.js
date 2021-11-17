8d829a63caa7ca8f249c63b55271fcd615842eb2
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-11-17 03:56:26 +0300

    Update autoplay.js

--- commands/🎶 Music/autoplay.js
+++ commands/🎶 Music/autoplay.js
@@ -4,7 +4,7 @@ var config = require("../../config/config.json");
 const distube = require("../../utils/distubeClient");
 
 module.exports = {
-  name: "autoplay",
+  name: "/autoplay",
   aliases: ["auplay", "autop"],
   category: "🎶 Music",
   permissions: " ",
