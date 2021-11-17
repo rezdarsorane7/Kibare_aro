52994c03278e2daa7cfe1a4b5e35b385b37d2e90
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-11-17 04:38:25 +0300

    Update antivc.js

--- commands/🚫 Administration/antivc.js
+++ commands/🚫 Administration/antivc.js
@@ -3,7 +3,7 @@ var ee = require('../../config/embed.json');
 var config = require('../../config/config.json');
 
 module.exports = {
-    name: 'antivc',
+    name: '/antivc',
     aliases: ['novc'],
     category: '🚫 Administration',
     memberpermissions: ['MANAGE_CHANNELS'],
