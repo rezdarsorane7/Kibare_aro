77936eada6940ea46cf43ec714c07d89069e4f4b
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-11-17 03:54:40 +0300

    Update nowplaying.js

--- commands/🎶 Music/nowplaying.js
+++ commands/🎶 Music/nowplaying.js
@@ -4,7 +4,7 @@ var config = require("../../config/config.json");
 const distube = require("../../utils/distubeClient");
 
 module.exports = {
-    name: "nowplaying",
+    name: "/nowplaying",
     aliases: ["nowplay"],
     category: "🎶 Music",
     permissions: " ",
