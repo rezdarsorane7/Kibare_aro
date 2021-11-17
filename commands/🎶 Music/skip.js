663ce4399e4cde5fe8575b1847d78a20123a4b21
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-11-17 03:47:59 +0300

    Update skip.js

--- commands/🎶 Music/skip.js
+++ commands/🎶 Music/skip.js
@@ -4,7 +4,7 @@ var config = require("../../config/config.json");
 const distube = require("../../utils/distubeClient");
 
 module.exports = {
-  name: "/ skip /",
+  name: "/skip",
   aliases: ["sk"],
   category: "🎶 Music",
   permissions: " ",
