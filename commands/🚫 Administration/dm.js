48aa333d0afcde77ea39fd87018fc1de784ddcb7
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-11-17 04:36:20 +0300

    Update dm.js

--- commands/🚫 Administration/dm.js
+++ commands/🚫 Administration/dm.js
@@ -3,7 +3,7 @@ var ee = require('../../config/embed.json');
 var config = require('../../config/config.json');
 
 module.exports = {
-    name: 'dm',
+    name: '/dm',
     aliases: ['userdm'],
     category: '🚫 Administration',
     memberpermissions: ['ADMINISTRATOR'],
