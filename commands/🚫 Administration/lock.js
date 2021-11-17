9227f7dd09fd38fbc1915241c6ce2ad8990f4ac0
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-11-17 04:33:52 +0300

    Update lock.js

--- commands/🚫 Administration/lock.js
+++ commands/🚫 Administration/lock.js
@@ -4,7 +4,7 @@ var config = require('../../config/config.json');
 const ms = require('ms')
 
 module.exports = {
-    name: 'lock',
+    name: '/lock',
     aliases: ['pl'],
     category: '🚫 Administration',
     memberpermissions: ['MANAGE_CHANNELS'],
