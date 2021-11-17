2b48478636f9c684d889765ed32151368bfdf648
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-11-17 04:34:45 +0300

    Update hackban.js

--- commands/🚫 Administration/hackban.js
+++ commands/🚫 Administration/hackban.js
@@ -3,7 +3,7 @@ var ee = require('../../config/embed.json');
 var config = require('../../config/config.json');
 
 module.exports = {
-    name: 'hackban',
+    name: '/hackban',
     aliases: ['bns'],
     category: '🚫 Administration',
     memberpermissions: ['ADMINISTRATOR'],
