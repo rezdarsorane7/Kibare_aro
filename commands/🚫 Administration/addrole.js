44f0f7b944f1d9352034a2e4c4f95170f8f0c1f1
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-11-17 04:24:26 +0300

    Update addrole.js

--- commands/🚫 Administration/addrole.js
+++ commands/🚫 Administration/addrole.js
@@ -3,7 +3,7 @@ var ee = require('../../config/embed.json');
 var config = require('../../config/config.json');
 
 module.exports = {
-    name: 'addrole',
+    name: '/addrole',
     aliases: ['giverole'],
     category: '🚫 Administration',
     memberpermissions: ['MANAGE_ROLES'],
