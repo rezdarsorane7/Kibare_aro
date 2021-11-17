ac01c185460b79d6b310e24b0b5f713ff073879c
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-11-17 04:29:17 +0300

    Update textchannel.js

--- commands/🚫 Administration/textchannel.js
+++ commands/🚫 Administration/textchannel.js
@@ -3,7 +3,7 @@ var ee = require('../../config/embed.json');
 var config = require('../../config/config.json');
 
 module.exports = {
-    name: 'textchannel',
+    name: '/textchannel',
     aliases: ['ch'],
     category: '🚫 Administration',
     memberpermissions: ['MANAGE_CHANNELS'],
