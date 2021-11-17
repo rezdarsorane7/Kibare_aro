4a3e588a0d6093ed0ed6ab0b4a5e4004a37f4548
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-11-17 04:37:24 +0300

    Update bans.js

--- commands/🚫 Administration/bans.js
+++ commands/🚫 Administration/bans.js
@@ -3,7 +3,7 @@ var ee = require('../../config/embed.json');
 var config = require('../../config/config.json');
 
 module.exports = {
-    name: 'bans',
+    name: '/bans',
     aliases: ['bns'],
     category: '🚫 Administration',
     memberpermissions: ['ADMINISTRATOR'],
