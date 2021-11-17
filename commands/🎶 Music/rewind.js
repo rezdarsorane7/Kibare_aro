3e75d07ee947003ff52c8864ea7e8824b20a2201
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-11-17 03:50:58 +0300

    Update rewind.js

--- commands/🎶 Music/rewind.js
+++ commands/🎶 Music/rewind.js
@@ -4,7 +4,7 @@ var config = require("../../config/config.json");
 const distube = require("../../utils/distubeClient");
 
 module.exports = {
-  name: "rewind",
+  name: "/rewind",
   aliases: ["rw"],
   category: "🎶 Music",
   permissions: " ",
