2fbde9eb3477cf9033d6805c5460c48e006974f0
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-11-17 04:04:14 +0300

    Update eval.js

--- commands/Owner/eval.js
+++ commands/Owner/eval.js
@@ -3,7 +3,7 @@ var ee = require('../../config/embed.json');
 var config = require('../../config/config.json');
 
 module.exports = {
-    name: 'eval',
+    name: '/eval',
     aliases: [''],
     category: ' ',
     memberpermissions: [],
